49593966522ce73590cd1fa732b93de2a357aeab
MinhHo <61931543+minhho89@users.noreply.github.com>
2022-04-11 16:59:55 +0700

    Validate_Subsquence O(n)

--- src/com/minhho/arrays/ValidateSubSequence.java
+++ src/com/minhho/arrays/ValidateSubSequence.java
@@ -1,10 +1,24 @@
 package com.minhho.arrays;
 
+import java.util.List;
+
 /**
  * By MinhHO 2022/04/11
  */
 public class ValidateSubSequence {
+    // O(n)
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        int seqIdx = 0;
+        for (Integer i: array) {
+            if (seqIdx == sequence.size()) break;
 
+            if (sequence.get(seqIdx) == i) {
+                seqIdx++;
+            }
 
+        }
+
+        return seqIdx == sequence.size();
+    }
 
 }
